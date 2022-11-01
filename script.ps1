330adc21fe8efebd5e918c2e40ab6a0afad21e64
MendelCruz <109176092+MendelCruz@users.noreply.github.com>
2022-11-01 17:24:17 -0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -6,7 +6,7 @@ jobs:
     name: Run Script powershell
     runs-on: ubuntu-latest
     steps:
-      - uses: actions/checkout@v1
-      - name: Script
+      uses: actions/checkout@v1
+      name: Script
         run: ./script.ps1
         shell: pwsh
